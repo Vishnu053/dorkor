00ed9ea89ccd04fe1a5da9c61192082064ff6f63
Vishnu <vishnu@descpro.com>
2020-03-13 14:14:46 +0530

    Changes

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,13 @@
         </v-list>
       </v-navigation-drawer>
 
-      <v-app-bar
+      <!-- <v-app-bar
       app
       clipped-left
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+      <v-toolbar-title>Dorker</v-toolbar-title>
+    </v-app-bar> -->
 
     <v-content>
       <router-view/>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img :src="require('../assets/logo.svg')"
+        <v-img :src="require('../assets/logo.png')"
                class="my-3"
                contain
                height="200" />
@@ -17,18 +17,18 @@
 
       <v-col class="mb-5"
              cols="12">
-        <h2 class="headline font-weight-bold mb-3">
+        <!-- <h2 class="headline font-weight-bold mb-3">
           What's next?
-        </h2>
+        </h2> -->
 
         <v-row justify="center">
           <v-col cols="12" sm="6">
           <v-text-field
               v-model="message"
-              label="Enter your wish"
+              label="What Are You Looking For?"
               outlined
               clearable
-              @keyup.enter="search"
+              @keydown.enter="search"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -43,6 +43,7 @@
       </v-col>
 
     </v-row>
+    <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> -->
   </v-container>
 
 </template>
@@ -57,7 +58,7 @@
     }),
     methods:{
       search(){
-        
+
       }
     }
 
