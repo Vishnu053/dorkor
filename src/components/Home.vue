c8a9339b9458f60ba05d8d0b790fe1e95d46a7be
Vishnu <vishnu@descpro.com>
2020-03-13 12:53:54 +0530

    ui

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img :src="require('../assets/logo.svg')"
+               class="my-3"
+               contain
+               height="200" />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h3 class="display-2 font-weight-bold mb-3">
+          Dorkor
+        </h3>
+      </v-col>
+
+      <v-col class="mb-5"
+             cols="12">
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <v-col cols="12" sm="6">
+          <v-text-field
+              v-model="message"
+              label="Enter your wish"
+              outlined
+              clearable
+              @keyup.enter="search"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col class="mb-5"
+             cols="12">
+        <v-icon small class=" mb-3">
+          mdi-help
+        </v-icon>
+
+      </v-col>
+
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      message:""
+    }),
+    methods:{
+      search(){
+        
+      }
+    }
+
+  }
+
+</script>
