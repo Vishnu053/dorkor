d47908dd73bee685aed474fb85b33c5b1b361146
Vishnu <39244822+Vishnu053@users.noreply.github.com>
2020-03-21 16:56:45 +0530

    Added logic for searchbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -74,6 +74,96 @@
     methods:{
       search(){
         // display results in an iframe
+        //logic used in searchbar
+         var ot = 0,
+ nt = 0,
+ qmoff = 0,
+ qmon = 0,
+ invrt = 0,
+ hue = 0,
+ revert = 0,
+ dashboard=0
+ for (var i of th) {
+ if (i == "new" || i == "theme") {
+ nt++;
+ }
+ if (i == "old" || i == "theme") {
+ ot++;
+ }
+ if (i == "quick" || i == "menu") {
+ qmoff++;
+ qmon++;
+ }
+ if (i == "disable" || i == "off" || i == "hide") {
+ qmoff++;
+ }
+ if (i == "enable" || i == "on" || i == "show") {
+ qmon++;
+ }
+ if (i == "invert" || i == "dark" || i == "night" || i == "black") {
+ invrt++;
+ }
+ if (i == "hue") {
+ hue++;
+ }
+ if (i == "revert" || i == "normal" || i == "reset") {
+ revert++;
+ }
+ if (i=="dashboard") {
+ dashboard++
+ }
+ if (i=="assistant" || i=="butler" || i=="dpassistant" || i=="dp assistant") {
+   this.searchString = ""
+   this.assistantSLideOutShow=true
+ }
+ }
+ // handle menuList
+ if (qmon > qmoff) {
+ localStorage.setItem("quickMenu", 1);
+ this.getSearchParamList();
+ this.searchString = "";
+ }
+ if (qmoff > qmon) {
+ localStorage.setItem("quickMenu", 0);
+ this.searchParamList = [];
+ this.searchString = "";
+ }
+ // end handle menuList
+ // theming
+ if (nt >= 2) {
+ localStorage.setItem(btoa("theme"), btoa("new"));
+ this.searchString = "";
+ this.loadThemeSettings();
+ }
+ if (ot >= 2) {
+ localStorage.setItem(btoa("theme"), btoa("old"));
+ this.searchString = "";
+ this.loadThemeSettings();
+ }
+ if (invrt >= 1) {
+ localStorage.setItem(btoa("invert"), btoa(1));
+ this.loadThemeSettings();
+ this.searchString = "";
+ }
+ if (hue >= 1) {
+ localStorage.setItem(btoa("hue"), btoa(1));
+ $('body').css("filter","hue-rotate(45deg)")
+ document.getElementsByClassName(
+ "navbar-static-side"
+ )[0].style.background = "#001c3cba";
+ this.searchString = "";
+ }
+ if (revert >= 1) {
+ localStorage.setItem(btoa("invert"), btoa(0));
+ localStorage.setItem(btoa("hue"), btoa(0));
+ this.loadThemeSettings();
+ this.searchString = "";
+ }
+ //end logic  
+        
+        
+        
+        
       }
     }
 
